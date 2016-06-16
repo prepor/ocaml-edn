73cfa5a269c02c4100f1973d61c802bcbee3fadc
Marek Kubica <marek@xivilization.net>
2016-06-16 21:52:05 +0200

    Implement returning a stream of EDN results
    This mirrors pretty much the stream_of_channel from Yojson.
    
    Closes #3

--- src/reader.ml
+++ src/reader.ml
@@ -8,3 +8,10 @@ let from_string s =
 
 let from_channel ch =
   parse (Lexing.from_channel ch)
+
+let stream_from_channel ch =
+  let lexbuf = Lexing.from_channel ch in
+  Stream.from @@ fun _ ->
+    match parse lexbuf with
+    | v -> Some v
+    | exception End_of_file -> None
