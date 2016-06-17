36b9e0fb68be4ec8c2429cbb382d024164fe8d11
Jonas Enlund <jonas.enlund@gmail.com>
2016-06-17 06:28:44 +0300

    Use _ for unused args

--- src/reader.ml
+++ src/reader.ml
@@ -11,5 +11,5 @@ let from_channel ch =
 
 let stream_from_channel ch =
   let lexbuf = Lexing.from_channel ch in
-  let f i = Edn_parser.prog Read.read lexbuf in
+  let f _ = Edn_parser.prog Read.read lexbuf in
   Stream.from f
